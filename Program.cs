e3942df29f08b1630922665a47f6efe6585eeb57
alex <alexandergaray171@gmail.com>
2025-04-12 14:07:11 -0500

    se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double pagofijoHora = 11.50;
+
+            Console.WriteLine("Registro de Sueldo con Horas Extras");
+
+            Console.Write("Ingrese el nombre del trabajador: ");
+            string Trabajador = Console.ReadLine();
+
+            Console.Write("Ingrese su sueldo mensual: S/ ");
+            double sueldoMensual = double.Parse(Console.ReadLine());
+
+            Console.Write("Horas Extras Trabajadas: ");
+            int horasExtras = int.Parse(Console.ReadLine());
+
+            double MontoExtra = horasExtras * pagofijoHora;
+            double sueldoTotal = sueldoMensual + MontoExtra;
+
+            Console.WriteLine(" el sueldo de {0} al mes sera de : {1} * {2} = {3} + {4} = {5}" , 
+            Trabajador , horasExtras, pagofijoHora , MontoExtra , sueldoMensual , sueldoTotal );
+
+            Console.ReadKey();
+        }
+    }
+}
+   
+
+
+
+
+
+
+
---
+++ RRHH.md
@@ -0,0 +1,3 @@
+Este proyecto tiene como objetivo automatizar el calculo 
+del salario mensual de los trabajadores de la empresa MyT Consultores
+, considerando el sueldo mensual base , y las horas extras trabajadas.
